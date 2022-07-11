3283c5dd16376e07d7ad33603039c56f2793ab6b
Pratik <paulpratik6@gmail.com>
2022-07-11 14:15:26 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>hi</h1>
       {/* Clock */}
       {/* List 1  initialValues [1, 2, 3] */}
       {/* List 2  initialValues [4, 5] */}
